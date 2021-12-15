eda97476161873d0b2ff75187dec8781fda78e61
Jillium <86424914+Jillium@users.noreply.github.com>
2021-12-15 11:23:06 -0700

    signup functionality

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -29,4 +29,12 @@ router.get('/', (rez, res) => {
     
 });
 
+router.get('/login', (req, res) => {
+    res.render('login');
+});
+
+router.get('/signup', (req, res) => {
+    res.render('signup');
+});
+
 module.exports = router;
