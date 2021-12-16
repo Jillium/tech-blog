f86344543162fc1088d4395ba160f1ce669d902d
Jillium <86424914+Jillium@users.noreply.github.com>
2021-12-16 10:58:31 -0700

    progress save, single post not rendering properly

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -46,4 +46,46 @@ router.get('/', (req, res) => {
     console.log(req.session);
 });
 
+router.get('/post/:id', (req, res) => {
+  Post.findOne({
+      where: {
+          id: req.params.id
+      },
+      attributes: [
+          'id',
+          'title',
+          'content',
+          'created_at'
+      ],
+      include: [
+          {
+              model: Comment,
+              attributes: ['id', 'text', 'post_id', 'user_id', 'created_at'],
+              include: {
+                  model: User,
+                  attributes: ['username']
+              }
+          },
+          {
+              model: User,
+              attributes: ['username']
+          }
+      ]
+  })
+  .then(dbPostData => {
+      if (!dbPostData) {
+          res.status(404).json({ message: 'No post found with this id' });
+          return;
+      }
+      const post = dbPostData.get({ plain: true });
+      res.render('single-post', { post });
+  })
+  .catch(err => {
+      console.log(err);
+      res.status(500).json(err);
+  });
+   
+  });
+  
+
 module.exports = router;
