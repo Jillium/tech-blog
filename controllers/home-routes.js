97b5eb99a00dfdabd93a1dbfa63bae4a67433d9f
Jillium <86424914+Jillium@users.noreply.github.com>
2021-12-16 11:02:49 -0700

    progress save

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -46,45 +46,67 @@ router.get('/', (req, res) => {
     console.log(req.session);
 });
 
-router.get('/post/:id', (req, res) => {
-  Post.findOne({
+router.get('/login', (req, res) => {
+    if (req.session.loggedIn) {
+      res.redirect('/');
+      return;
+    }
+    res.render('login');
+  });
+  
+  
+  // route for session/cookies
+  router.get('/', (req, res) => {
+    console.log(req.session);
+  
+    // other logic...
+  });
+  
+  //route for single post page
+  router.get('/post/:id', (req, res) => {
+    Post.findOne({
       where: {
-          id: req.params.id
+        id: req.params.id
       },
       attributes: [
-          'id',
-          'title',
-          'content',
-          'created_at'
+        'id',
+        'title',
+        'content',
+        'created_at',
+        
       ],
       include: [
-          {
-              model: Comment,
-              attributes: ['id', 'text', 'post_id', 'user_id', 'created_at'],
-              include: {
-                  model: User,
-                  attributes: ['username']
-              }
-          },
-          {
-              model: User,
-              attributes: ['username']
+        {
+          model: Comment,
+          attributes: ['id', 'text', 'post_id', 'user_id', 'created_at'],
+          include: {
+            model: User,
+            attributes: ['username']
           }
+        },
+        {
+          model: User,
+          attributes: ['username']
+        }
       ]
-  })
-  .then(dbPostData => {
+    })
+    .then(dbPostData => {
       if (!dbPostData) {
-          res.status(404).json({ message: 'No post found with this id' });
-          return;
+        res.status(404).json({ message: 'No post found with this id' });
+        return;
       }
+  
+      // serialize the data
       const post = dbPostData.get({ plain: true });
-      res.render('single-post', { post });
-  })
-  .catch(err => {
+  
+      // pass data to the template
+      res.render('single-post', { post, loggedIn: req.session.loggedIn });
+  
+    })
+    .catch(err => {
       console.log(err);
       res.status(500).json(err);
-  });
-   
+    });
   });
   
 
