109c6d86a19290f92743e465a1be5bffe24e44b4
Jillium <86424914+Jillium@users.noreply.github.com>
2021-12-21 09:16:04 -0700

    homepage redirect fix

--- public/javascript/signup.js
+++ public/javascript/signup.js
@@ -18,7 +18,7 @@ async function signupFormHandler(event) {
 
         if (response.ok) {
             console.log('success');
-            document.location.replace('/homepage');
+            document.location.replace('/');
 
         } else {
             alert(response.statusText);
